d056dce67bbf2a5c64422e1febeea2d429ee27f8
KidoPlay <kidoplay@qq.com>
2015-10-10 17:24:52 +0800

    Fix __BEGIN_DECLS __END_DECLS

--- src/WProtect/ELF/elf.h
+++ src/WProtect/ELF/elf.h
@@ -21,6 +21,14 @@
 
 #include "features.h"
 
+#if defined(__cplusplus)
+       #define __BEGIN_DECLS extern "C" {
+       #define __END_DECLS }
+       #else
+       #define __BEGIN_DECLS
+       #define __END_DECLS
+#endif
+
 __BEGIN_DECLS
 
 /* Standard ELF types.  */
